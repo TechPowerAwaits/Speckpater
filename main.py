2522a218d0b849c1ea856d2bd99114d8f60450c9
Richard J <techpowerawaits@outlook.com>
2021-04-26 20:31:21 -0600

    Disable invalid joysticks
    The code will now loop around, looking for valid joysticks. Invalid
    joysticks would include those which lack the appropriate amount of
    buttons. It would also include joysticks which appear to be always
    triggering a direction change, thus rendering the game unplayable.
    
    Also, a related deprecated function has been dealt with.

--- main.py
+++ main.py
@@ -71,11 +71,40 @@ class Main(engine.Game):
 		
 		#Joystick initialization
 		if (pygame.joystick.get_count() > 0): #Seems like there is a virtual joystick in some computer
-			print "Detected Joystick"
-			base.has_joy = True
-			base.joy = pygame.joystick.Joystick(0)
-			base.joy.init()
-
+			for joy_id in range(pygame.joystick.get_count()):
+				base.joy = pygame.joystick.Joystick(joy_id)
+				if pygame.version.vernum[0] < 2:
+					# Deprecated since version 2.0.0 of pygame.
+					base.joy.init()
+				# Need to check whether the joystick is functional or not.
+				horz_test = False
+				vert_test = False
+				if base.joy.get_numbuttons() < 2:
+					# No point in using the joystick if it is lacking buttons.
+					pass
+				elif base.joy.get_numaxes() >= 2:
+					horz_test = abs(base.joy.get_axis(0)) < 0.1
+					vert_test = abs(base.joy.get_axis(1)) < 0.1
+				else:
+					pass
+				
+				if horz_test and vert_test:
+					print "Valid Joystick Detected: #" + str(joy_id)
+					base.has_joy = True
+					break
+				else:
+					base.joy.quit()
+					base.joy = None
+					print
+					print "Joystick #" + str(joy_id) + " has been disabled, as it appeared faulty."
+					print "If the Joystick is known to be working, check if a button was being pushed down during startup."
+					print "If it is still not working, please file a bug at https://github.com/TechPowerAwaits/Speckpater."
+					print
+			if not base.has_joy:
+				print
+				print "No Valid Joysticks Detected."
+				print
+		
 		print self.settings
 		print base.DATA
 	
