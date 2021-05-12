d208d70db5757d91467489aa31e88aa2efa3bfa9
Richard J <techpowerawaits@outlook.com>
2021-05-12 13:06:10 -0600

    Rename level files
    The entire level structure was overhauled. All level files were placed
    in a speckpater folder in preparation for levelset support.

--- levels/tilesets/__init__.py
+++ levels/tilesets/__init__.py
@@ -2,8 +2,8 @@
 
 
 """
-__version__ = '0.1'
+__version__ = '0.2'
 
 # add all the script file names here for the * import
-__all__ = ["cavetiles", "commontiles","templetiles", "mountaintiles"]
+__all__ = ["cave", "common","temple", "mountain"]
 
